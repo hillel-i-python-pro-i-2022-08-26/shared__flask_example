cbe73da01ab8be612863ba44890c80335cc75c6a
alirex <alirex.prime@gmail.com>
2022-09-16 22:42:56 +0300

    ✨ Add extra tools
    Black, pre-commit, etc

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import random
-from typing import Iterator
+from collections.abc import Iterator
 
 from flask import Flask
 from webargs import fields
@@ -12,12 +12,12 @@ def get_items_as_iterator(amount: int = 20) -> Iterator[int]:
     return (random.randint(0, 100) for _ in range(amount))
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():  # put application's code here
-    return 'Hello World! 13'
+    return "Hello World! 13"
 
 
-@app.route('/example')
+@app.route("/example")
 def example():  # put application's code here
     # [parse_input]-[BEGIN]
     # Nothing for now
@@ -28,46 +28,27 @@ def example():  # put application's code here
     # [run_main_handler]-[END]
 
     # [format_output]-[BEGIN]
-    list_items_formatted = ''.join(f'<li>{item}</li>' for item in items)
+    list_items_formatted = "".join(f"<li>{item}</li>" for item in items)
     # return f'<ul>{list_items_formatted}</ul>'
-    return f'<ol>{list_items_formatted}</ol>'
+    return f"<ol>{list_items_formatted}</ol>"
     # [format_output]-[BEGIN]
 
     # return ''.join(f'<p>{random.randint(0, 100)}</p>' for _ in range(20))
 
 
-@app.route('/greetings/<name>/<int:age>')
+@app.route("/greetings/<name>/<int:age>")
 def greetings_by_path(name: str, age: int):  # put application's code here
-    return f'Hi, {name}. You are {age} old.'
+    return f"Hi, {name}. You are {age} old."
 
 
-@app.route('/greetings-2')
+@app.route("/greetings-2")
 @use_args({"name": fields.Str(required=True), "age": fields.Int(required=True)}, location="query")
 def greetings_by_query(args):  # put application's code here
     name: str = args["name"]
     age: int = args["age"]
 
-    return f'Hi, {name}. You are {age} old.'
+    return f"Hi, {name}. You are {age} old."
 
 
-# div
-# p
-# span
-# a
-
-# ul
-# ol
-# li
-
-# form
-# button
-# input
-
-# h1,h2
-
-# br
-# hr
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
